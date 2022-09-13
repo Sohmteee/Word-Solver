e8744fd4589a5269afac09b982280d1638ea1b22
Sohmteee <sohmtee@gmail.com>
2022-09-13 21:36:28 +0100

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: const HomePage(),
-      darkTheme: fal,
+      debugShowCheckedModeBanner: false,
     );
   }
 }
