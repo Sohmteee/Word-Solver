cab553e736155def135107e8c4ab0022183e3c7c
Sohmteee <sohmtee@gmail.com>
2022-09-16 22:42:38 +0100

    commit

--- lib/widgets/size_textfield.dart
+++ lib/widgets/size_textfield.dart
@@ -1,16 +1,33 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
-class Size extends StatefulWidget {
-  const Size({Key? key}) : super(key: key);
+import '../data.dart';
+
+class SizeTextField extends StatefulWidget {
+  const SizeTextField({Key? key}) : super(key: key);
 
   @override
-  State<Size> createState() => _SizeState();
+  State<SizeTextField> createState() => _SizeTextFielddState();
 }
 
-class _SizeState extends State<Size> {
+class _SizeTextFielddState extends State<SizeTextField> {
   @override
   Widget build(BuildContext context) {
-    
-  }
-}
+    return Container(
+      width: 100,
+      margin: const EdgeInsets.symmetric(vertical: 20),
+      padding: const EdgeInsets.symmetric(
+        horizontal: 20,
+      ),
+      decoration: BoxDecoration(
+        color: Colors.grey.shade100,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: TextField(
+        controller: word,
+        autofocus: true,
+        decoration: InputDecoration(
+            fillColor: Colors.green.shade100,
+            border: InputBorder.none,
+            hintText: "Min"),
