e2a9fdfebf746709aefbbc93eb83963409bd4a32
Sohmteee <sohmtee@gmail.com>
2022-09-16 22:41:34 +0100

    commit

---
+++ lib/widgets/size_textfield.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Size extends StatefulWidget {
+  const Size({Key? key}) : super(key: key);
+
+  @override
+  State<Size> createState() => _SizeState();
+}
+
+class _SizeState extends State<Size> {
+  @override
+  Widget build(BuildContext context) {
+    
+  }
+}
