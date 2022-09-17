b8bd42e02e8cc24531cd69c17d5e74a478676538
Sohmteee <sohmtee@gmail.com>
2022-09-17 03:48:19 +0100

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter/material.dart';
 import 'homepage.dart';
 
-Future<void> main() async {
+void main() async {
   runApp(const MyApp());
 }
 
