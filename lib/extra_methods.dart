de54fbc6f2c969539b5d5f5fe22bf08f45cbe760
Sohmteee <sohmtee@gmail.com>
2022-09-17 04:40:22 +0100

    commit

--- lib/extra_methods.dart
+++ lib/extra_methods.dart
@@ -2,7 +2,7 @@ Map<String, int> getCharacterCount(String letters) {
   Map<String, int> countLetters = {};
 
   for (int i = 0; i < letters.length; i++) {
-    char currentLetter = letters.charAt(i);
+    String currentLetter = letters[i];
     int count = countLetters.getOrDefault(currentLetter, 0);
     countLetters.put(currentLetter, count + 1);
   }
