e6ed27a508076ae13892c81818789dcdc9da1984
Sohmteee <sohmtee@gmail.com>
2022-09-17 07:59:21 +0100

    commit

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -18,11 +18,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     Widget displayResult() {
       List<String> r = [];
-      if (letters != "" &&
-          (((size.runtimeType == List<int>) &&
-                  ((min.text.trim() != "" || min.text.isNotEmpty) &&
-                      (max.text.trim() != "" || max.text.isNotEmpty))) ||
-              (size.runtimeType == int))) {
+      if (letters != "") {
         Map<String, int> lettersMap = getCharacterCount(letters.toLowerCase());
         print(letters);
         print(lettersMap);
