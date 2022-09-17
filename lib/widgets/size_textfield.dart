b0f83066433e24e420026bbc290f8ce837c3b1fb
Sohmteee <sohmtee@gmail.com>
2022-09-17 08:28:58 +0100

    commit

--- lib/widgets/size_textfield.dart
+++ lib/widgets/size_textfield.dart
@@ -37,7 +37,7 @@ class _SizeTextFielddState extends State<SizeTextField> {
         textAlign: TextAlign.center,
         keyboardType: TextInputType.number,
         onChanged: (value) => setState(() {
-          widget.controller.text = value;
+          widget.controller = value;
           displayResult();
         }),
         decoration: InputDecoration(
