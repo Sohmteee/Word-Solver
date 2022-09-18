94a507f5a75531b542e0ea9651be4c87d2ed0f64
Sohmteee <sohmtee@gmail.com>
2022-09-18 10:10:55 +0100

    commit

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -72,7 +72,6 @@ class _HomePageState extends State<HomePage> {
                     onChanged: (String? value) {
                       setState(() {
                         selectedSize = value;
-                        displayResult();
                       });
                     },
                   ),
