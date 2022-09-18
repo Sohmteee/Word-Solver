41baf407ff5b11e5727b70cb0653d0af9669c927
Sohmteee <sohmtee@gmail.com>
2022-09-18 14:06:30 +0100

    commit

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -37,6 +37,7 @@ class _HomePageState extends State<HomePage> {
                   onChanged: (value) {
                     setState(() {
                       letters = value;
+                      displayResult();
                     });
                   },
                   decoration: InputDecoration(
